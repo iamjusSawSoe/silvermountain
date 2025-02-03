bb7ec89c955a10d8a053aa6271cbcce563284ae2
iamjusSawSoe <sawsoehtutwin78@gmail.com>
2025-02-03 17:07:35 +0630

    add more pages

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -1,5 +1,84 @@
+import { TextField } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  return (
+    <div
+      className="h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url('/assets/service/1.jpg')" }}
+    >
+      <div className="text-white  w-[75%] mx-auto h-full">
+        <div className="grid grid-cols-[40%_60%] gap-8 pt-16">
+          <div>
+            <h1 className="text-7xl font-bold mb-2">Contact Us</h1>
+            <h2 className="text-3xl font-bold mb-6">Singapore</h2>
+            <p className="text-lg mb-8 whitespace-pre-line tracking-wide">
+              We're here to move mountains for you. Reach out{" "}
+              <span className="block"></span> today to get personalized support,
+              request a <span className="block"></span> quote, or inquire about
+              our products and services.
+            </p>
+
+            <p className=" text-lg font-bold mb-4">SILVER MOUNTAIN PTE LTD</p>
+            <p className="text-lg mb-4">
+              7 Temasek Boulevard, #12-07 <span className="block"></span>{" "}
+              Singapore 038987
+            </p>
+            <p className="text-lg mb-4">Tel: Your Number</p>
+            <p className="text-lg">Email: silvermountainpteltd@gmail.com</p>
+          </div>
+
+          <div className="bg-white rounded-2xl p-10">
+            <Grid container spacing={6}>
+              <Grid size={{ xs: 6, md: 6 }}>
+                <TextField
+                  fullWidth
+                  id="filled-basic"
+                  label="Name"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid size={{ xs: 6, md: 6 }}>
+                <TextField
+                  fullWidth
+                  id="filled-basic"
+                  label="Email"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid size={{ xs: 6, md: 6 }}>
+                <TextField
+                  fullWidth
+                  id="filled-basic"
+                  label="Phone Number"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid size={{ xs: 6, md: 6 }}>
+                {" "}
+                <TextField
+                  fullWidth
+                  id="filled-basic"
+                  label="Name"
+                  variant="standard"
+                />
+              </Grid>
+
+              <Grid size={12}>
+                {" "}
+                <TextField
+                  fullWidth
+                  id="filled-basic"
+                  label="Share your inquiry with us"
+                  variant="standard"
+                />
+              </Grid>
+            </Grid>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactUs;
--- app/services/page.js
+++ app/services/page.js
@@ -28,9 +28,9 @@ const Services = () => {
       </div>
 
       <div className="w-[75%] mx-auto py-20">
-        <section className="grid grid-cols-[40%_55%] gap-8 items-center justify-center">
+        <section className="grid grid-cols-[45%_55%] gap-2  justify-center">
           {/* Image Section */}
-          <div className="w-full h-[500px] flex">
+          <div className="w-full h-full flex">
             <Image
               src="/assets/service/2.png"
               alt="silver mountain services"
@@ -43,13 +43,30 @@ const Services = () => {
           </div>
 
           {/* Text Section */}
-          <div>
+          <div className="mt-10">
             <h1 className="text-5xl font-bold text-primary">Services</h1>
-            <ul className="mt-4 pl-6 font-bold text-lg leading-8 list-disc">
-              <li>Engine Oil</li>
-              <li>Cooking Oil</li>
-              <li>High Speed Diesel</li>
-              <li>Automotive Diesel (EN590)</li>
+            <ul className="mt-8 pl-5 pr-16 font-bold text-lg leading-9 list-disc">
+              <li>
+                <span className="text-primary">Bulk Order Handling: </span>
+                We manage both small and large orders, offering flexibility and
+                competitive pricing to suit your needs.
+              </li>
+              <li>
+                <span className="text-primary">Logistics & Shipping: </span>
+                Our efficient logistics team ensures that your order reaches you
+                on time, no matter the distance.
+              </li>
+              <li>
+                <span className="text-primary">Consultation: </span>
+                We provide expert advice on product sourcing, quality checks,
+                and market trends to guide your purchasing decisions.
+              </li>
+              <li>
+                <span className="text-primary">Customized Solutions: </span>
+                Whether you need specialized products or unique delivery terms,
+                we offer flexible solutions that cater to your specific business
+                needs.
+              </li>
             </ul>
           </div>
         </section>

1ec2b17702fac1177a9b34984b36bcec3c254db7
Saw Soe <sawsoehtutwin78@gmail.com>
2025-02-03 07:55:21 +0630

    add core values section

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { FaCircle } from "react-icons/fa";
 
 const AboutUs = () => {
   return (
@@ -37,8 +38,8 @@ const AboutUs = () => {
         <Image
           src={"/assets/about_us/1.jpg"}
           alt="silver mountain about us"
-          width={1000}
-          height={500}
+          width={400}
+          height={400}
           className=""
         />
         <div>
@@ -56,6 +57,78 @@ const AboutUs = () => {
             Silver Mountain, we envision a world where our solutions help every
             partner reach new heights of success.
           </p>
+        </div>
+      </div>
+
+      {/* start of core values */}
+      <div>
+        <h1 className="text-5xl font-bold text-primary mt-16">
+          Core Values (R.I.S.E)
+        </h1>
+        <div className="grid grid-cols-[65%_35%] gap-6 mt-6 w-full items-center tracking-tight">
+          {/* Text Content (70%) */}
+          <div className="space-y-2">
+            <div className="flex gap-2">
+              <div className="w-6 mt-[0.6rem]">
+                <FaCircle fontSize={12} className="text-primary" />
+              </div>
+              <span className="mt-[3px] font-bold text-lg">
+                <span className="text-primary">Resilience: </span>
+                At Silver Mountain, resilience is the bedrock of everything we
+                do. Just like a mountain stands firm against the elements, we
+                remain steadfast in overcoming challenges and adapting to market
+                volatility.
+              </span>
+            </div>
+            <div className="flex gap-2">
+              <div className="w-6 mt-[0.6rem]">
+                <FaCircle fontSize={12} className="text-primary" />
+              </div>
+              <span className="mt-[3px] font-bold text-lg">
+                <span className="text-primary">Integrity: </span>
+                Integrity is the foundation of everything we do. We believe in
+                honest, transparent business practices and building trust with
+                our clients and partners. Upholding ethical standards is crucial
+                to maintaining long-term, meaningful relationships.
+              </span>
+            </div>
+            <div className="flex gap-2">
+              <div className="w-6 mt-[0.6rem]">
+                <FaCircle fontSize={12} className="text-primary" />
+              </div>
+              <span className="mt-[3px] font-bold text-lg">
+                <span className="text-primary">Sustainability: </span>
+                At Silver Mountain, we are committed to protecting the planet
+                while driving global commerce. Sustainability is at the heart of
+                our operations, from sourcing responsibly to optimizing
+                eco-friendly logistics.
+              </span>
+            </div>
+            <div className="flex gap-2">
+              <div className="w-6 mt-[0.6rem]">
+                <FaCircle fontSize={12} className="text-primary" />
+              </div>
+              <span className="mt-[3px] font-bold text-lg">
+                <span className="text-primary">Excellence: </span>
+                We strive for excellence in all that we do. Whether it's the
+                quality of our products, the efficiency of our services, or the
+                strength of our customer relationships, we aim to exceed
+                expectations.
+              </span>
+            </div>
+          </div>
+
+          {/* Image (30%) */}
+          <div className="w-full">
+            <Image
+              src="/assets/about_us/2.jpg"
+              alt="silver mountain about us"
+              width={800}
+              height={400}
+              layout="responsive"
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 30vw"
+            />
+          </div>
         </div>
       </div>
     </div>
